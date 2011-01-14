be49225dc72c91b391f23fb8d2704645915f2de0
Sandeep Shinde <sashinde@nvidia.com>
2011-01-14 11:19:50 +0530

    IOMX: Fix broken backward compatibility
    IOMX interface is currently broken for 3rd party applications which are
    compiled against standard google headers.
    
    Moving the custom added function to the end of interface will make sure that
    such apps run fine on Tegra devices.
    
    Bug 779979
    
    Change-Id: I29d23869b75b37227b54329db44ea6c520144967

--- include/media/IOMX.h
+++ include/media/IOMX.h
@@ -79,11 +79,6 @@ public:
             node_id node, OMX_INDEXTYPE index,
             const void *params, size_t size) = 0;
 
-    virtual status_t useEGLImage(
-            node_id node, OMX_U32 port_index,
-            const sp<GraphicBuffer> &graphic_buffer,
-            buffer_id *buffer) = 0;
-
     virtual status_t useBuffer(
             node_id node, OMX_U32 port_index, const sp<IMemory> &params,
             buffer_id *buffer) = 0;
@@ -142,6 +137,11 @@ public:
             size_t encodedWidth, size_t encodedHeight,
             size_t displayWidth, size_t displayHeight,
             int32_t rotationDegrees);
+
+    virtual status_t useEGLImage(
+            node_id node, OMX_U32 port_index,
+            const sp<GraphicBuffer> &graphic_buffer,
+            buffer_id *buffer) = 0;
 };
 
 struct omx_message {
